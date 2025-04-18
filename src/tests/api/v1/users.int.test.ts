9151bdbc18b507561bbbf795bee421bb691eaf68
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-04-18 16:38:27 +0200

    Test getting all users

--- src/tests/api/v1/users.int.test.ts
+++ src/tests/api/v1/users.int.test.ts
@@ -1,12 +1,21 @@
+import { User } from '../../../../prisma/generated/client';
+import { NewDefaultUser } from '../../../types';
+import { describe, it, expect } from 'vitest';
 import express from 'express';
 import request from 'supertest';
+import db from '../../../lib/db';
 import usersRouter from '../../../api/v1/users';
-import { describe, it, expect } from 'vitest';
 
 const app = express();
 app.use('/', usersRouter);
 
 const api = request(app);
+
+const userMock: NewDefaultUser = {
+  fullname: 'Clark Kent/Kal-El',
+  username: 'superman',
+  password: 'Ss@12312',
+};
 
 describe('/users', () => {
   describe('get all users', () => {
@@ -16,6 +25,18 @@ describe('/users', () => {
       expect(res.statusCode).toBe(200);
       expect(res.body).toEqual(expect.arrayContaining([]));
       expect(res.body).toHaveLength(0);
+    });
+
+    it('should return the correct user list', async () => {
+      const { id } = await db.user.create({ data: userMock });
+      const res = await api.get('/');
+      const users = res.body as User[];
+      expect(res.type).toMatch(/json/);
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toHaveLength(1);
+      expect(users[0].username).toStrictEqual(userMock.username);
+      expect(users[0].fullname).toStrictEqual(userMock.fullname);
+      await db.user.delete({ where: { id } });
     });
   });
 });
