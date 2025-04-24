90d873ebd8ce0a5824fe3f57c36500ccd8ef3b4f
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-04-24 10:55:33 +0200

    Refactor the request-logger middleware

--- src/middlewares/request-logger.ts
+++ src/middlewares/request-logger.ts
@@ -1,14 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
-import { CI } from '../lib/config';
 import logger from '../lib/logger';
 
-export const logReq = (req: Request, res: Response, next: NextFunction) => {
-  if (CI) {
-    logger.info(`${req.method}: ${req.originalUrl}`);
-  } else {
-    logger.info(`${req.method}: ${req.originalUrl}`, req.body);
-  }
+export const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const body = req.body as unknown;
+  logger.info(`${req.method}: ${req.originalUrl}`, { body });
   next();
 };
 
-export default logReq;
+export default requestLogger;
