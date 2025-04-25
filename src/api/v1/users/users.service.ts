c45512b9517e70cc7c8b896e99d8d44336715633
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-04-25 16:38:01 +0300

    Refactor the code in users service

--- src/api/v1/users/users.service.ts
+++ src/api/v1/users/users.service.ts
@@ -8,80 +8,82 @@ import { NewUserOutput, PublicUser } from '../../../types';
 import { SALT } from '../../../lib/config';
 import db from '../../../lib/db';
 import bcrypt from 'bcryptjs';
+import { catchDBKnownError } from '../../../lib/helpers';
 
 const omit = { password: true, isAdmin: true };
 
 const hashPassword = (password: string) => bcrypt.hash(password, SALT);
 
-export default {
-  async getAll() {
-    return await db.user.findMany();
-  },
+export const getAll = async () => await db.user.findMany();
 
-  async createOne(newUser: NewUserOutput): Promise<PublicUser> {
-    try {
-      newUser.password = await hashPassword(newUser.password);
-      return await db.user.create({ data: newUser, omit });
-    } catch (error) {
-      if (
-        error instanceof Prisma.PrismaClientKnownRequestError &&
-        error.code === 'P2002'
-      ) {
-        const targets = error.meta?.target as string[] | undefined;
-        throw new AppUniqueConstraintViolationError(
-          targets?.at(-1) ?? 'username'
-        );
-      }
-      throw error;
+export const createOne = async (
+  newUser: NewUserOutput
+): Promise<PublicUser> => {
+  const data = { ...newUser };
+  data.password = await hashPassword(data.password);
+  const [user, dbKnownError] = await catchDBKnownError(
+    db.user.create({ data, omit })
+  );
+  if (dbKnownError) {
+    if (dbKnownError.code === 'P2002') {
+      const targets = dbKnownError.meta?.target as string[] | undefined;
+      throw new AppUniqueConstraintViolationError(
+        targets?.at(-1) ?? 'username'
+      );
     }
-  },
-
-  async findOneById(id: string): Promise<PublicUser | null> {
-    try {
-      return await db.user.findUnique({ where: { id }, omit });
-    } catch (error) {
-      if (
-        error instanceof Prisma.PrismaClientKnownRequestError &&
-        error.code === 'P2023'
-      ) {
-        throw new AppInvalidIdError();
-      }
-      throw error;
-    }
-  },
-
-  async updateOne(id: string, data: Prisma.UserUpdateInput): Promise<void> {
-    try {
-      if (data.password && typeof data.password === 'string') {
-        data.password = await hashPassword(data.password);
-      }
-      await db.user.update({ where: { id }, data, omit });
-    } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === 'P2023') throw new AppInvalidIdError();
-        if (error.code === 'P2025') {
-          throw new AppNotFoundError('user not found');
-        }
-        if (error.code === 'P2002') {
-          const targets = error.meta?.target as string[] | undefined;
-          throw new AppUniqueConstraintViolationError(
-            targets?.at(-1) ?? 'username'
-          );
-        }
-      }
-      throw error;
-    }
-  },
-
-  async deleteOne(id: string): Promise<void> {
-    try {
-      await db.user.delete({ where: { id }, omit });
-    } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === 'P2023') throw new AppInvalidIdError();
-        if (error.code === 'P2025') return;
-      }
-      throw error;
-    }
-  },
+    throw dbKnownError;
+  }
+  return user;
 };
+
+export const findOneById = async (id: string): Promise<PublicUser | null> => {
+  const [user, dbKnownError] = await catchDBKnownError(
+    db.user.findUnique({ where: { id }, omit })
+  );
+  if (dbKnownError) {
+    if (dbKnownError.code === 'P2023') throw new AppInvalidIdError();
+    throw dbKnownError;
+  }
+  return user;
+};
+
+export const updateOne = async (
+  id: string,
+  userData: Prisma.UserUpdateInput
+): Promise<void> => {
+  const data = { ...userData };
+  if (data.password && typeof data.password === 'string') {
+    data.password = await hashPassword(data.password);
+  }
+  const dbKnownError = (
+    await catchDBKnownError(db.user.update({ where: { id }, data, omit }))
+  )[1];
+  if (dbKnownError) {
+    if (dbKnownError.code === 'P2023') {
+      throw new AppInvalidIdError();
+    }
+    if (dbKnownError.code === 'P2025') {
+      throw new AppNotFoundError('user not found');
+    }
+    if (dbKnownError.code === 'P2002') {
+      const targets = dbKnownError.meta?.target as string[] | undefined;
+      throw new AppUniqueConstraintViolationError(
+        targets?.at(-1) ?? 'username'
+      );
+    }
+    throw dbKnownError;
+  }
+};
+
+export const deleteOne = async (id: string): Promise<void> => {
+  const error = (
+    await catchDBKnownError(db.user.delete({ where: { id }, omit }))
+  )[1];
+  if (error) {
+    if (error.code === 'P2023') throw new AppInvalidIdError();
+    if (error.code === 'P2025') return;
+    throw error;
+  }
+};
+
+export default { getAll, createOne, findOneById, updateOne, deleteOne };
