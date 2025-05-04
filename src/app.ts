5a75a9c0094dd35384db2e21e7649b667b17d56d
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-05-04 12:11:27 +0300

    Add environment-based CORS origin allowlist
    Add environment-based CORS origin allowlist that should be passed
    within the `ALLOWED_ORIGINS` environment variable
    as a comma-separated list of origins that.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import requestLogger from './middlewares/request-logger';
 import errorHandler from './middlewares/error-handler';
+import { ALLOWED_ORIGINS } from './lib/config';
 import AppError from './lib/app-error';
 import apiV1Router from './api/v1';
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
+import logger from './lib/logger';
 
 const app = express();
 
@@ -12,6 +15,14 @@ app.disable('x-powered-by');
 app.use(helmet());
 app.use(express.json());
 app.use(requestLogger);
+
+logger.info('ALLOWED_ORIGINS: ', ALLOWED_ORIGINS);
+app.use(
+  cors({
+    origin: ALLOWED_ORIGINS,
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 
 app.use('/api/v1', apiV1Router);
 
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,12 @@
 if (!process.env.NODE_ENV) console.warn('Miss Env Var: NODE_ENV');
 if (!process.env.SECRET) console.error('Missing Env Var: SECRET');
 if (!process.env.ADMIN_SECRET) console.error('Missing Env Var: ADMIN_SECRET');
+if (!process.env.ALLOWED_ORIGINS)
+  console.log('Missing Env Var: ALLOWED_ORIGINS');
+
+export const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(',').map((origin) => origin.trim())
+  : [];
 
 export const SALT = (Number(process.env.SALT) || process.env.SALT) ?? 10;
 export const SECRET = process.env.SECRET ?? 'secret';
@@ -9,4 +15,12 @@ export const TOKEN_EXP_PERIOD = process.env.TOKEN_EXP_PERIOD ?? '3d';
 export const NODE_ENV = process.env.NODE_ENV;
 export const CI = Boolean(process.env.CI);
 
-export default { SALT, SECRET, ADMIN_SECRET, TOKEN_EXP_PERIOD, NODE_ENV, CI };
+export default {
+  TOKEN_EXP_PERIOD,
+  ALLOWED_ORIGINS,
+  ADMIN_SECRET,
+  NODE_ENV,
+  SECRET,
+  SALT,
+  CI,
+};
